0e7ec20f1ea2aa9c13b7750b366a5b811b502886
Andrey Gruzdev <agruzdev1@gmail.com>
2024-08-07 18:02:42 +0200

    refactor: refactor action handler

--- src/mappings/mappingHandlers.ts
+++ src/mappings/mappingHandlers.ts
@@ -2,38 +2,29 @@ import { NearAction } from '@subql/types-near'
 import { NearActionEntity } from '../types/models'
 
 export async function handleAction(action: NearAction): Promise<void> {
-  // An Action can belong to either a transaction or a receipt
-  // To check which one, we can check if action.transaction is null
-  // If it is null, then it belongs to a receipt
-
-  logger.info(
-    `Handling action at ${
-      action.transaction
-        ? action.transaction.block_height
-        : action.action.block_height
-    }`
-  )
-
-  const id = action.transaction
+  const isTransaction = !!action.transaction
+  const blockHeight = isTransaction
+    ? action.transaction.block_height
+    : action.action.block_height
+  const id = isTransaction
     ? `${action.transaction.block_height}-${action.transaction.result.id}-${action.id}`
     : `${action.action.block_height}-${action.action.id}-${action.id}`
-  const sender = action.transaction
+  const sender = isTransaction
     ? action.transaction.signer_id
     : action.action.predecessor_id
-  const receiver = action.transaction
+  const receiver = isTransaction
     ? action.transaction.receiver_id
     : action.action.receiver_id
+  const txHash = isTransaction ? action.transaction.result.id : action.action.id
+
+  logger.info(`Handling action at ${blockHeight}`)
 
   const actionRecord = NearActionEntity.create({
     id: id,
     sender: sender,
     receiver: receiver,
-    txHash: action.transaction
-      ? action.transaction.result.id
-      : action.action.id,
-    blockNumber: action.transaction
-      ? action.transaction.block_height
-      : action.action.block_height,
+    txHash: txHash,
+    blockNumber: blockHeight,
   })
 
   await actionRecord.save()
